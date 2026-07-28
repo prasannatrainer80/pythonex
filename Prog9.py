4dc748f2529ab222ec285e383503fc3e1a494ac2
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-07-28 11:16:37 +0530

    code

---
+++ Prog9.py
@@ -0,0 +1,11 @@
+sno=int(input("Enter a Number  "))
+if sno==1:
+    print("Hi i am Vedavathi...")
+elif sno==2:
+    print("Hi I am Pavan...")
+elif sno==3:
+    print("Hi I am Madhu...")
+elif sno==4:
+    print("Hi I am Vamsi...")
+else:
+    print("Unknown Sno")
