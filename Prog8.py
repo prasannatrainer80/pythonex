e9f5bb506ea039d471d1eb42b6d9af26596c1f00
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-07-28 11:11:35 +0530

    code

---
+++ Prog8.py
@@ -0,0 +1,7 @@
+# Program to check a person elligible for voting
+# or not
+age=int(input("Enter Your Age   "))
+if age >= 18:
+    print("You Are Elligible for Voting...")
+else:
+    print("You are Not-Elligible for Voting...")
