a47d0aac69ce251a75f6015409f5ad8ba9df9823
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-07-28 11:24:34 +0530

    code

---
+++ Prog10.py
@@ -0,0 +1,12 @@
+choice=int(input("Enter Your Choice  "))
+match choice:
+    case 1:
+        print("Hi I am Vamsi...")
+    case 2 :
+        print("Hi I am Madhu...")
+    case 3 :
+        print("Hi I am Vedhavathi...")
+    case 4 :
+        print("Hi I am Pavan...")
+    case _ :
+        print("Invalid Choice...")
